ea3ddbce74e5881e1f8ec2a68593748f89e5c86b
mishramartand9 <31269808+mishramartand9@users.noreply.github.com>
2019-06-26 14:57:58 +0530

    Sunshine network

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -70,7 +70,12 @@ public final class NetworkUtils {
      */
     public static URL buildUrl(String locationQuery) {
         // Completed (1) Fix this method to return the URL used to query Open Weather Map's API
-        Uri weatherMapUri= Uri.parse(FORECAST_BASE_URL).buildUpon().appendQueryParameter(QUERY_PARAM,locationQuery).build();
+        Uri weatherMapUri= Uri.parse(FORECAST_BASE_URL).buildUpon().
+                 appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
         URL weatherMapUrl=null;
         try
         {
